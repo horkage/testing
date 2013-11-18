de02d4989ec826e9f029b241b87caa7582f4a3ec
Michael Wood <horkage@gmail.com>
2013-11-18 23:17:55 +0000

    initial commit

---
+++ app/views/pets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @pet, :name, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hork::Application.config.secret_key_base = '900cec43d9188a8c41e71ce9fa65ed5250650ab58011bdbc61f239c34195eebcfe743bf1a0a5f92512f9f131be3d384bb6371422a68dd2bae877845f58acce5e'
